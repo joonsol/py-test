5dc6078e67799911eecff50b7387cc3152ee7e55
joonsol <71648638+joonsol@users.noreply.github.com>
2025-05-21 21:55:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
+st.title("Strealit Test")
 
+name =st.text_input("이름을 입력하세요")
 
-st.title("streamlit test")
-st.text("hello python!!")
+if st.button("확인"):
+    st.success(f"{name}님 안녕하세요!")
